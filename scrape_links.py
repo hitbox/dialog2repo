9050fe3cd88104a3a392d26b6976e48cac4a3921
Carl L. Harris <Carl.Harris@airborneglobal.com>
2025-01-02 10:42:16 -0500

    Development on scraping links.
    - Use detected config as defaults for argument parser.
    - Was trying to get requests to work with another network.

--- scrape_links.py
+++ scrape_links.py
@@ -1,14 +1,19 @@
 import argparse
 import ast
+import configparser
 import fnmatch
 import os
 import pickle
 import re
 import unicodedata
 
+import certifi
 import requests
 
 from bs4 import BeautifulSoup
+
+app_name = 'scrape_links'
+environ_config_key = 'SCRAPE_LINKS_CONFIG'
 
 class Any:
     """
@@ -32,52 +37,59 @@ def main(argv=None):
     """
     Command line interface.
     """
+    # Detect config path and use them as defaults for the argument parser.
+    config = configparser.ConfigParser()
+    if environ_config_key in os.environ:
+        config.read(os.environ[environ_config_key])
+
+    default_url = config.get(app_name, 'url', fallback=None)
+    default_tag_name = config.get(app_name, 'tag-name', fallback=None)
+    default_href = []
+    if config.has_section(app_name):
+        for key in config[app_name]:
+            if key.startswith('href') and key[4:].isdigit():
+                default_href.append(wildcard_re(config[app_name][key]))
+    default_prepend_url = config.get(app_name, 'prepend-url', fallback=None)
+
     parser = argparse.ArgumentParser(
         description = 'Scrape links for Akesson\'s Dialog.',
+        epilog =
+            'Config path found in SCRAPE_LINKS_CONFIG environment variable.',
     )
     parser.add_argument(
         '--url',
-        default = 'https://linusakesson.net/dialog/index.php',
-        help = 'URL to scrape Linus Akesson\'s Dialog links.',
+        default = default_url,
+        help = 'URL to scrape Linus Akesson\'s Dialog links. Default: %(default)s',
     )
     parser.add_argument(
         '--tag-name',
-        default = 'a',
+        default = default_tag_name,
+        help = 'HTML element tag to look for. Default: %(default)r',
     )
     parser.add_argument(
         '--href',
         action = 'append',
-        default = [
-            '*dialog-*.zip',
-            '*dialog-*.dg',
-        ],
-        help = 'Pattern(s) match the url.',
+        default = default_href,
+        type = wildcard_re,
+        help = 'Pattern(s) match the url. Default: %(default)s',
     )
     parser.add_argument(
         '--prepend-url',
-        default = 'https:',
-        help = 'Prepend this to the scraped urls.',
-    )
-    parser.add_argument(
-        '--cache',
-        default = '.dialog-cache.pickle',
-        help = 'Path to save data from previous run.',
+        default = default_prepend_url,
+        help = 'Prepend this to the scraped urls. Default: %(default)s',
     )
     args = parser.parse_args(argv)
 
     url = args.url
     tag_name = args.tag_name
     prepend_url = args.prepend_url
-    cache_path = args.cache
 
     if not args.href:
         # Match any elements having an href attribute.
         href_pattern = True
     else:
         # BeautifulSoup uses the .search method of regexes.
-        href_regexes = [regex.search for regex in map(wildcard_re, args.href)]
-        href_pattern = Any(href_regexes)
-        del href_regexes
+        href_pattern = Any([regex.search for regex in args.href])
 
     dialog_items = []
     response = requests.get(url)
@@ -98,24 +110,6 @@ def main(argv=None):
 
     for download_url, message in dialog_items:
         print((download_url, message))
-
-    if os.path.exists(cache_path):
-        # Check for diffs
-        with open(cache_path, 'rb') as cache_file:
-            cache = pickle.load(cache_file)
-
-        if cache != dialog_items:
-            print('Different!')
-    else:
-        with open(cache_path, 'wb') as cache_file:
-            pickle.dump(dialog_items, cache_file)
-
-    # XXX
-    # - Arguments from config file.
-    # - Keep cache of downloaded files and git ignore them.
-    # - dialog-*.zip.message files for related messages?
-    # - How to know when new file comes in?
-    # - How to put into the real repo?
 
 if __name__ == '__main__':
     main()
